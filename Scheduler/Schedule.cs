ffceac49c70d28948b28712abfffeb48669f3037
Max <maksim_kashkin@mail.ru>
2020-01-21 00:13:35 +0300

    Create Task and Schedule classes. Realese crud methods

---
+++ Scheduler/Schedule.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler
+{
+    class Schedule
+    {
+        public List<Task> Tasks { get; set; }
+
+        public void AddTask(string nameTask, DateTime completionDate)
+        {
+            Tasks.Add(new Task(nameTask, completionDate));
+        }
+
+        public void AddTask(Task task)
+        {
+            Tasks.Add(task);
+        }
+
+        public void DeleteTask(string nameTask)
+        {
+            var task = Tasks.Find(e => e.Name.Equals(nameTask));
+            if (task != null)
+            {
+                Tasks.Remove(task);
+            }
+        }
+
+        public void DeleteTask(DateTime completionDate)
+        {
+            var task = Tasks.Find(e => e.CompletionDate.Equals(completionDate));
+            if (task != null)
+            {
+                Tasks.Remove(task);
+            }
+        }
+
+        public void EditTask(string nameTask, Task newTask)
+        {
+            var task = Tasks.Find(e => e.Name.Equals(nameTask));
+            if (task != null)
+            {
+                task.Name = newTask.Name;
+                task.CompletionDate = newTask.CompletionDate;
+            }
+        }
+
+        public string PrintTasksList()
+        {
+            return Tasks.Aggregate("", (current, item) => current + item.ToString() + "\n");
+        }
+    }
+}
---
+++ Scheduler/Task.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler
+{
+    class Task
+    {
+        private DateTime creationDate;
+
+        public string Name { get; set; }
+        public DateTime CreationDate { get; }
+        public DateTime CompletionDate { get; set; }
+
+        public Task(string Name, DateTime CompletionDate)
+        {
+            this.Name = Name;
+            this.creationDate = DateTime.Now;
+            this.CompletionDate = CompletionDate;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Task Name: {0}\t Creation Date: {1}\t Completion Date: {2}",
+                this.Name, this.CreationDate, this.CompletionDate);
+        }
+    }
+}
